37cd71160caadb2a8a555560c56cc26a70b5db60
NERD-X <nerdxontheline@outlook.com>
2019-10-17 13:40:05 +0100

    Fix TimetoString

--- src/main/java/WorldTime/WorldTime.java
+++ src/main/java/WorldTime/WorldTime.java
@@ -13,11 +13,11 @@ public class WorldTime {
         if (country.compareToIgnoreCase("Japan") == 0) {
             DateTimeZone zone = DateTimeZone.forID("Asia/Tokyo");
             LocalTime dt = new LocalTime(zone);
-            return dt.getHourOfDay()+":"+dt.getMinuteOfHour();
+            return dt.toString("HH:mm");
         } else if(country.compareToIgnoreCase("America/New_York") == 0){
             DateTimeZone zone = DateTimeZone.forID("America/New_York");
             LocalTime dt = new LocalTime(zone);
-            return dt.getHourOfDay()+":"+dt.getMinuteOfHour();
+            return dt.toString("HH:mm");
         }
         else {
             System.out.println("País ainda não foi adicionado ao sistema.");
